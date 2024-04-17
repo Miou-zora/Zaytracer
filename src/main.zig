ce30f1e95ecb330b55a0aa5189523da6f881441a
Alexandre Flion <huntears@kreog.com>
2024-04-17 11:07:42 +0200

    Fix types in Frame

--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,18 @@ const std = @import("std");
 const zlm = @import("zlm");
 
 const Frame = struct {
-    height: f32,
-    width: f32,
+    const Self = @This();
+
+    height: u32,
+    width: u32,
     pixels: []u8,
 
-    pub fn init(height: f32, width: f32, allocator: *const std.mem.Allocator) !Frame {
+    pub fn init(height: u32, width: u32, allocator: *const std.mem.Allocator) !Self {
         var self: Frame = undefined;
 
         self.height = height;
         self.width = width;
-        self.pixels = try allocator.alloc(u8, @as(usize, @intFromFloat(height * width * 4)));
+        self.pixels = try allocator.alloc(u8, @as(usize, height * width * 4));
 
         return self;
     }
