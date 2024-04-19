01c618f4845620321259b75612b0d7e01f4de9a2
Alexandre Flion <huntears@kreog.com>
2024-04-19 15:57:23 +0200

    Add simple qoi encoding + Remove ppm encoding
    This is very temporary and WILL be subject to change soon:tm:
    So it's normal if it looks like shit.
    
    It also misses the license from zig-qoi which will be added soon
    too (I sure do hope before merging this in main...)

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const Vec3 = @import("Vec3.zig").Vec3;
 const Ray = @import("Ray.zig").Ray;
 const Sphere = @import("Sphere.zig").Sphere;
 const Camera = @import("Camera.zig").Camera;
+const qoi = @import("qoi.zig");
 
 pub fn main() !void {
     const camera = Camera{
@@ -26,26 +27,51 @@ pub fn main() !void {
         },
     };
     const sphere = Sphere{ .center = .{ .x = 0, .y = 0, .z = 2 }, .radius = 0.5 };
-    const max_y = 100;
-    const max_x = 100;
-    var buffered_out = std.io.bufferedWriter(std.io.getStdOut().writer());
-    var out = buffered_out.writer();
-    try out.print("P3\n", .{});
-    try out.print("{d} {d}\n", .{ max_x, max_y });
-    try out.print("255\n", .{});
-    for (0..max_y) |y| {
-        for (0..max_x) |x| {
-            const scaled_x: f32 = @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(max_x));
-            const scaled_y: f32 = @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(max_y));
+    const height = 1000;
+    const width = 1000;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+    var image = qoi.Image{
+        .width = std.math.cast(u32, width) orelse return error.Overflow,
+        .height = std.math.cast(u32, height) orelse return error.Overflow,
+        .colorspace = .sRGB,
+        .pixels = try allocator.alloc(qoi.Color, width * height),
+    };
+    defer image.deinit(allocator);
+
+    var index: usize = 0;
+    for (0..height) |y| {
+        for (0..width) |x| {
+            const scaled_x: f32 = @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(width));
+            const scaled_y: f32 = @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(height));
             const ray: Ray = camera.createRay(scaled_x, scaled_y);
             if (sphere.hits(ray)) {
-                try out.print("255 0 0\n", .{});
+                image.pixels[index] = .{
+                    .r = 255,
+                    .g = 0,
+                    .b = 0,
+                    .a = 255,
+                };
             } else {
-                try out.print("255 255 255\n", .{});
+                image.pixels[index] = .{
+                    .r = 255,
+                    .g = 255,
+                    .b = 255,
+                    .a = 255,
+                };
             }
+            index += 1;
         }
     }
-    try buffered_out.flush();
+
+    var file = try std.fs.cwd().createFile("out.qoi", .{});
+    defer file.close();
+
+    const buffer = try qoi.encodeBuffer(allocator, image.asConst());
+    defer allocator.free(buffer);
+    try file.writeAll(buffer);
 }
 
 test {
