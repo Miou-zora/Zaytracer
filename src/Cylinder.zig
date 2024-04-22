9a55d155f9e02e898752decf8bddeb81e284b2e3
Miouzora <alexandre.franquet@epitech.eu>
2024-04-22 20:35:01 +0200

    Remove commen,t

--- src/Cylinder.zig
+++ src/Cylinder.zig
@@ -10,55 +10,6 @@ pub const Cylinder = struct {
     radius: f32,
     origin: Pt3,
 
-    // RayTracer::HitRecord hit_record;
-
-    // double a = pow(ray._direction._x, 2) + pow(ray._direction._y, 2);
-    // double b = 2 * (ray._direction._x * (ray._origin._x - this->getPosition()._x) +
-    //                 ray._direction._y * (ray._origin._y - this->getPosition()._y));
-    // double c = pow((ray._origin._x - this->getPosition()._x), 2) +
-    //            pow((ray._origin._y - this->getPosition()._y), 2) -
-    //            pow(_radius, 2);
-    // double discriminant = b * b - 4 * a * c;
-
-    // hit_record.setHit(false);
-    // if (discriminant < 0)
-    //     return hit_record;
-    // if (discriminant == 0) {
-    //     double t = -b / (2 * a);
-    //     hit_record.setIntersectionPoint(ray._origin + ray._direction * t);
-    //     double distance = Maths::MathsUtils::distance(ray._origin, hit_record.getIntersectionPoint());
-    //     if (distance < 0)
-    //         return hit_record;
-    //     hit_record.setHit(true);
-    //     hit_record.setDistance(distance);
-    //     hit_record.setNormal(Maths::Vector(hit_record.getIntersectionPoint()._x - this->getPosition()._x,
-    //                                        hit_record.getIntersectionPoint()._y - this->getPosition()._y,
-    //                                        0));
-    //     hit_record.setFrontFace(ray._direction.dot(hit_record.getNormal()) < 0);
-    //     hit_record.setMaterial(this->getMaterial());
-
-    //     return hit_record;
-    // }
-    // double t1 = (-b + sqrt(discriminant)) / (2 * a);
-    // double t2 = (-b - sqrt(discriminant)) / (2 * a);
-    // if (t1 < 0 && t2 < 0) {
-    //     hit_record.setHit(false);
-    //     return hit_record;
-    // }
-    // double t = (abs(t1) < abs(t2)) ? t1 : t2;
-    // hit_record.setIntersectionPoint(ray._origin + ray._direction * t);
-    // double distance = Maths::MathsUtils::distance(ray._origin, hit_record.getIntersectionPoint());
-    // hit_record.setDistance(distance);
-    // if (distance < 0)
-    //     return hit_record;
-    // hit_record.setHit(true);
-    // hit_record.setNormal(Maths::Vector(hit_record.getIntersectionPoint()._x - this->getPosition()._x,
-    //                                     hit_record.getIntersectionPoint()._y - this->getPosition()._y,
-    //                                     0));
-    // hit_record.setFrontFace(true);
-    // hit_record.setMaterial(this->getMaterial());
-    // return hit_record;
-
     pub fn hits(self: *const Self, ray: Ray) HitRecord {
         const rx_minus_cx = ray.origin.x - self.origin.x;
         const ry_minus_cy = ray.origin.y - self.origin.y;
