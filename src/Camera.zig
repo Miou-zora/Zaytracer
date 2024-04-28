89fb8d6720f193fc0a33e0c77463e40cf6bb7383
Miouzora <alexandre.franquet@epitech.eu>
2024-04-28 22:30:33 +0200

    Add better camera

--- src/Camera.zig
+++ src/Camera.zig
@@ -6,13 +6,23 @@ const std = @import("std");
 pub const Camera = struct {
     const Self = @This();
 
-    origin: Pt3,
-    screen: Rect3,
+    width: u32,
+    height: u32,
+    fov: f32,
 
     pub fn createRay(self: *const Self, x: f32, y: f32) Ray {
+        const aspectRatio = @as(f32, @floatFromInt(self.height)) / @as(f32, @floatFromInt(self.width));
+        const halfWidth = aspectRatio / 2;
+        const TO_RAD = std.math.pi / 180.0;
+        const distance = (halfWidth * @sin((180.0 - self.fov / 2 - 90.0) * TO_RAD)) / @sin(self.fov * TO_RAD);
+        const screen = Rect3{
+            .origin = Pt3{ .x = -0.5, .y = -halfWidth, .z = distance },
+            .left = Pt3{ .x = 1, .y = 0, .z = 0 },
+            .top = Pt3{ .x = 0, .y = aspectRatio, .z = 0 },
+        };
         return Ray{
-            .origin = self.origin,
-            .direction = self.screen.pointAt(x, y).subVec3(self.origin),
+            .origin = Pt3.nil(),
+            .direction = screen.pointAt(x, y).subVec3(Pt3.nil()),
         };
     }
 };
