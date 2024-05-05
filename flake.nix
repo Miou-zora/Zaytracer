079bd4170deb000f0de2972d39e8c7e8c4bfd2b0
huntears <huntears@kreog.com>
2024-05-05 14:46:14 +0200

    Add zig-gamedev to flake
    Co-authored-by: Yohann Boniface <edhyjox@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zig-gamedev = {
+      url = "github:zig-gamedev/zig-gamedev";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, zig-gamedev }:
     flake-utils.lib.eachSystem [
       "aarch64-darwin"
       "aarch64-linux"
@@ -27,12 +31,16 @@
               raylib
             ];
           };
-          packages.default = pkgs.stdenv.mkDerivation {
+        packages.default = pkgs.stdenv.mkDerivation {
             name = "Zaytracer";
             src = ./.;
 
             XDG_CACHE_HOME = "${placeholder "out"}";
             
+            prePatch = ''
+              cp -r ${zig-gamedev} libs/zgamedev
+            '';
+
             buildInputs = [ pkgs.raylib ];
             buildPhase = ''
               ${pkgs.zig_0_12}/bin/zig build -Doptimize=ReleaseFast
@@ -42,6 +50,7 @@
               ${pkgs.zig_0_12}/bin/zig build install --prefix $out -Doptimize=ReleaseFast
               rm -rf $out/zig # remove cache
             '';
+            
           };
         });
 }
