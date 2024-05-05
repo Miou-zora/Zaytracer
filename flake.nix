7a5467fe3364ffae01b1fbe933a5b5c684022bdd
Sigmanificient <edhyjox@gmail.com>
2024-05-05 16:06:43 +0200

    Use alejandra for the flake formatter

--- flake.nix
+++ flake.nix
@@ -8,57 +8,59 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig-gamedev }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    zig-gamedev,
+  }:
     flake-utils.lib.eachSystem [
       "aarch64-darwin"
       "aarch64-linux"
       "x86_64-darwin"
       "x86_64-linux"
     ]
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        rec {
-          formatter = pkgs.nixpkgs-fmt;
-          devShells.default = pkgs.mkShell {
-            name = "Zaytracer";
-            inputsFrom = pkgs.lib.attrsets.attrValues packages;
-            nativeBuildInputs = with pkgs; [
-              linuxPackages_latest.perf
-              ffmpeg
-              hyperfine
-            ];
-          };
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      formatter = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        name = "Zaytracer";
+        inputsFrom = pkgs.lib.attrsets.attrValues packages;
+        nativeBuildInputs = with pkgs; [
+          linuxPackages_latest.perf
+          ffmpeg
+          hyperfine
+        ];
+      };
 
-          packages.default =
-            pkgs.stdenv.mkDerivation {
-              name = "Zaytracer";
-              src = ./.;
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "Zaytracer";
+        src = pkgs.lib.cleanSource ./.;
 
-              XDG_CACHE_HOME = "${placeholder "out"}";
+        XDG_CACHE_HOME = "${placeholder "out"}";
 
-              prePatch = ''
-                mkdir -p libs
+        prePatch = ''
+          mkdir -p libs
 
-                cp -r ${zig-gamedev} libs/zgamedev
-              '';
+          cp -r ${zig-gamedev} libs/zgamedev
+        '';
 
-              buildInputs = [ pkgs.raylib ];
-              nativeBuildInputs = [ pkgs.zig_0_12 ];
-              buildPhase = ''
-                zig build -Doptimize=ReleaseFast
-              '';
+        buildInputs = [pkgs.raylib];
+        nativeBuildInputs = [pkgs.zig_0_12];
+        buildPhase = ''
+          zig build -Doptimize=ReleaseFast
+        '';
 
-              doCheck = true;
-              checkPhase = ''
-                zig build test --summary all
-              '';
+        doCheck = true;
+        checkPhase = ''
+          zig build test --summary all
+        '';
 
-              installPhase = ''
-                zig build install --prefix $out -Doptimize=ReleaseFast
-                rm -rf $out/zig # remove cache
-              '';
-            };
-        });
+        installPhase = ''
+          zig build install --prefix $out -Doptimize=ReleaseFast
+          rm -rf $out/zig # remove cache
+        '';
+      };
+    });
 }
